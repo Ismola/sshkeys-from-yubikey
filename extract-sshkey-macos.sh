d37f94b1c030583550b74b3267209d8ffa77d170
Ismael Treviño <code@ismola.dev>
2025-06-19 10:09:53 +0200

    Agregar descarga y configuración automática del archivo SSH en scripts para Linux, macOS y Windows.

--- extract-sshkey-macos.sh
+++ extract-sshkey-macos.sh
@@ -4,3 +4,7 @@ cd ~/.ssh
 echo "Inserta tu YubiKey y toca el dispositivo cuando se te pida."
 ssh-keygen -K
 echo "Claves SSH extraídas desde la YubiKey."
+# Descargar el fichero config personalizado
+curl -fsSL https://raw.githubusercontent.com/Ismola/personal-ssh-config/main/config -o ~/.ssh/config
+chmod 600 ~/.ssh/config
+echo "Archivo de configuración SSH descargado y aplicado en ~/.ssh/config"
--- extract-sshkey-windows.ps1
+++ extract-sshkey-windows.ps1
@@ -2,3 +2,7 @@ Set-Location $env:USERPROFILE\.ssh
 Write-Host "Inserta tu YubiKey y toca el dispositivo cuando se te pida."
 ssh-keygen -K
 Write-Host "Claves SSH extraídas desde la YubiKey."
+# Descargar el fichero config personalizado
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Ismola/personal-ssh-config/main/config" -OutFile "$env:USERPROFILE\.ssh\config"
+icacls "$env:USERPROFILE\.ssh\config" /inheritance:r /grant:r "$($env:USERNAME):R"
+Write-Host "Archivo de configuración SSH descargado y aplicado en .ssh\config"
